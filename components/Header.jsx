c52d2cb2a5ceef048ca3b5c6f3d5d2bc10673b10
Rohan Kiran Adhav <155009973+Rohan-Adhav@users.noreply.github.com>
2025-08-12 19:55:02 +0530

    Add files via upload

---
+++ components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <header className="header">
+            <img src="/images/troll-face.png" alt="troll face" />
+            <h1>meme generator</h1>
+        </header>
+    )
+}
